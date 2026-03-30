CommunityToolkit/Aspire

Refactor TypeScript apphost validation
Co-authored-by: Copilot <223556219+Copilot@users.noreply.github.com>

tests/CommunityToolkit.Aspire.Hosting.Flyway.Tests/TypeScriptAppHostTests.cs
@@ -9,21 +9,12 @@ public sealed class TypeScriptAppHostTests
     [Fact]
     public async Task TypeScriptAppHostCompilesAndRunsFlywayTasks()
     {
-        string repoRoot = Path.GetFullPath(Path.Combine("..", "..", "..", "..", ".."));
-        string scriptPath = Path.Combine(repoRoot, "eng", "testing", "validate-typescript-apphost.ps1");
-        string appHostPath = Path.Combine(repoRoot, "examples", "flyway", "CommunityToolkit.Aspire.Hosting.Flyway.AppHost.TypeScript", "apphost.ts");
-        string packageProjectPath = Path.Combine(repoRoot, "src", "CommunityToolkit.Aspire.Hosting.Flyway", "CommunityToolkit.Aspire.Hosting.Flyway.csproj");
-        string shell = OperatingSystem.IsWindows() ? "pwsh.exe" : "pwsh";
-
-        await ProcessTestUtilities.RunProcessAsync(shell, [
-            "-NoLogo",
-            "-NoProfile",
-            "-File", scriptPath,
-            "-AppHostPath", appHostPath,
-            "-PackageProjectPath", packageProjectPath,
-            "-PackageName", "CommunityToolkit.Aspire.Hosting.Flyway",
-            "-WaitForResources", "flyway,flyway-telemetry",
-            "-WaitStatus", "down"
-        ], repoRoot, TestContext.Current.CancellationToken);
+        await TypeScriptAppHostTest.Run(
+            appHostProject: "CommunityToolkit.Aspire.Hosting.Flyway.AppHost.TypeScript",
+            packageName: "CommunityToolkit.Aspire.Hosting.Flyway",
+            exampleName: "flyway",
+            waitForResources: ["flyway", "flyway-telemetry"],
+            waitStatus: "down",
+            cancellationToken: TestContext.Current.CancellationToken);
     }
 }

alexbocharov/aspire-community-toolkit

tests/CommunityToolkit.Aspire.Hosting.Adminer.Tests/TypeScriptAppHostTests.cs
@@ -9,20 +9,11 @@ public class TypeScriptAppHostTests
     [Fact]
     public async Task TypeScriptAppHostCompilesAndStarts()
     {
-        string repoRoot = Path.GetFullPath(Path.Combine("..", "..", "..", "..", ".."));
-        string scriptPath = Path.Combine(repoRoot, "eng", "testing", "validate-typescript-apphost.ps1");
-        string appHostPath = Path.Combine(repoRoot, "examples", "adminer", "CommunityToolkit.Aspire.Hosting.Adminer.AppHost.TypeScript", "apphost.ts");
-        string packageProjectPath = Path.Combine(repoRoot, "src", "CommunityToolkit.Aspire.Hosting.Adminer", "CommunityToolkit.Aspire.Hosting.Adminer.csproj");
-        string shell = OperatingSystem.IsWindows() ? "pwsh.exe" : "pwsh";
-
-        await ProcessTestUtilities.RunProcessAsync(shell, [
-            "-NoLogo",
-            "-NoProfile",
-            "-File", scriptPath,
-            "-AppHostPath", appHostPath,
-            "-PackageProjectPath", packageProjectPath,
-            "-PackageName", "CommunityToolkit.Aspire.Hosting.Adminer",
-            "-WaitForResources", "adminer"
-        ], repoRoot, TestContext.Current.CancellationToken);
+        await TypeScriptAppHostTest.Run(
+            appHostProject: "CommunityToolkit.Aspire.Hosting.Adminer.AppHost.TypeScript",
+            packageName: "CommunityToolkit.Aspire.Hosting.Adminer",
+            exampleName: "adminer",
+            waitForResources: ["adminer"],
+            cancellationToken: TestContext.Current.CancellationToken);
     }
 }