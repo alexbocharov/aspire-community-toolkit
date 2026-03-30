CommunityToolkit/Aspire

Refactor TypeScript apphost validation
Co-authored-by: Copilot <223556219+Copilot@users.noreply.github.com>

tests/CommunityToolkit.Aspire.Hosting.LavinMQ.Tests/TypeScriptAppHostTests.cs
@@ -9,20 +9,11 @@ public class TypeScriptAppHostTests
     [Fact]
     public async Task TypeScriptAppHostCompilesAndStarts()
     {
-        string repoRoot = Path.GetFullPath(Path.Combine("..", "..", "..", "..", ".."));
-        string scriptPath = Path.Combine(repoRoot, "eng", "testing", "validate-typescript-apphost.ps1");
-        string appHostPath = Path.Combine(repoRoot, "examples", "lavinmq", "CommunityToolkit.Aspire.Hosting.LavinMQ.AppHost.TypeScript", "apphost.ts");
-        string packageProjectPath = Path.Combine(repoRoot, "src", "CommunityToolkit.Aspire.Hosting.LavinMQ", "CommunityToolkit.Aspire.Hosting.LavinMQ.csproj");
-        string shell = OperatingSystem.IsWindows() ? "pwsh.exe" : "pwsh";
-
-        await ProcessTestUtilities.RunProcessAsync(shell, [
-            "-NoLogo",
-            "-NoProfile",
-            "-File", scriptPath,
-            "-AppHostPath", appHostPath,
-            "-PackageProjectPath", packageProjectPath,
-            "-PackageName", "CommunityToolkit.Aspire.Hosting.LavinMQ",
-            "-WaitForResources", "volume-broker,bind-broker"
-        ], repoRoot, TestContext.Current.CancellationToken);
+        await TypeScriptAppHostTest.Run(
+            appHostProject: "CommunityToolkit.Aspire.Hosting.LavinMQ.AppHost.TypeScript",
+            packageName: "CommunityToolkit.Aspire.Hosting.LavinMQ",
+            exampleName: "lavinmq",
+            waitForResources: ["volume-broker", "bind-broker"],
+            cancellationToken: TestContext.Current.CancellationToken);
     }
 }

alexbocharov/aspire-community-toolkit

tests/CommunityToolkit.Aspire.Hosting.Bun.Tests/TypeScriptAppHostTests.cs
@@ -7,22 +7,13 @@ public class TypeScriptAppHostTests
     [Fact]
     public async Task TypeScriptAppHostCompilesAndStarts()
     {
-        string repoRoot = Path.GetFullPath(Path.Combine("..", "..", "..", "..", ".."));
-        string scriptPath = Path.Combine(repoRoot, "eng", "testing", "validate-typescript-apphost.ps1");
-        string appHostPath = Path.Combine(repoRoot, "examples", "bun", "CommunityToolkit.Aspire.Hosting.Bun.AppHost.TypeScript", "apphost.ts");
-        string packageProjectPath = Path.Combine(repoRoot, "src", "CommunityToolkit.Aspire.Hosting.Bun", "CommunityToolkit.Aspire.Hosting.Bun.csproj");
-        string shell = OperatingSystem.IsWindows() ? "pwsh.exe" : "pwsh";
-
-        await ProcessTestUtilities.RunProcessAsync(shell, [
-            "-NoLogo",
-            "-NoProfile",
-            "-File", scriptPath,
-            "-AppHostPath", appHostPath,
-            "-PackageProjectPath", packageProjectPath,
-            "-PackageName", "CommunityToolkit.Aspire.Hosting.Bun",
-            "-WaitForResources", "bun-app,bun-defaults,bun-watch",
-            "-WaitStatus", "up",
-            "-RequiredCommands", "bun"
-        ], repoRoot, TestContext.Current.CancellationToken);
+        await TypeScriptAppHostTest.Run(
+            appHostProject: "CommunityToolkit.Aspire.Hosting.Bun.AppHost.TypeScript",
+            packageName: "CommunityToolkit.Aspire.Hosting.Bun",
+            exampleName: "bun",
+            waitForResources: ["bun-app", "bun-defaults", "bun-watch"],
+            requiredCommands: ["bun"],
+            waitStatus: "up",
+            cancellationToken: TestContext.Current.CancellationToken);
     }
 }